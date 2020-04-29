b7b2b5010986586624d227286143bc6f750c7d7d
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2020-04-29 14:51:38 -0700

    Added minions.py with classes. Functionsplay_first() and combat() added.

--- combat.py
+++ combat.py
@@ -1,86 +1,11 @@
-from enum import Enum
 import random
 from random import choice
 import copy
-
-#do not create class of minions
-
-class MinionType(Enum):
-
-	PLAIN = 0
-	MURLOC = 1
-	DRAGON = 2
-	BEAST = 3
-	MECH = 4
-	DEMON = 5
-
-
-class Card:
-	name = None
-	life = None
-	attack = None
-	tier = None
-
-	def __init__(self, name, attack, life, tier, m_type):
-		self.name = name
-		self.attack = attack
-		self.life = life
-		self.tier = tier
-		self.m_type = m_type
+from minions import Card, Player
 
 
 
 
-class Event:
-	def __init__(self, evnt):
-		self.evnt = evnt
-
-#CHECKCHEKCCHECKCHEKCHCKLECHEKCHEKCHEKCHEKCHECK
-
-
-class AddingAttack(Event):
-	# murloc_warleader
-
-	is_alive = True
-	is_attack_added = False
-
-	def __init__(self, is_alive, additional_attack, objects_to_whom):
-		
-		for i in self.objects_to_whom:
-			i.attack += additional_attack
-
-		is_added = True
-
-class DoublingAttack(Event):
-	# glyph guardian
-
-	# is_attacking = True
-
-	def __init__(self, attack):
-		self.attack = 2*attack
-
-# class Taunt(Event):
-# 	is_attacked_first = True
-
-
-
-class StartOfCombat(Event):
-	# red whelp
-	is_attacking = True
-
-	def __init__(self, warband):
-		damage = sum(1 for i in self.warband if i.m_type == 2)
-
-
-
-
-
-class Player:
-
-	def __init__(self, name, warband):
-		self.name = name
-		self.warband = warband
-		# type of warband -> list
 
 # classes written:
 murloc_warleader = Card("Murloc Warleader", 3, 3, 2, 1)
@@ -103,8 +28,8 @@ selfless_hero = Card("Selfless Hero", 2, 1, 1, 0)
 # battlecry:
 rockpool_hunter = Card("Rockpool Hunter", 2, 3, 1, 1)
 
-alices_warband = [spawn_of_nzoth, dragonspawn_lieutenant, infested_wolf, murloc_warleader] 
-bobs_warband = [rockpool_hunter, righteous_protector, glyph_guardian, selfless_hero, red_whelp]
+alices_warband = [righteous_protector, dragonspawn_lieutenant] 
+bobs_warband = [red_whelp, selfless_hero, red_whelp]
 
 Player_1 = Player("Alice", alices_warband)
 Player_2 = Player("Bob", bobs_warband)
@@ -115,7 +40,6 @@ def attack(minion_1, minion_2):
 
 	minion_1.life -= minion_2.attack
 	minion_2.life -= minion_1.attack
-
 
 	return minion_1, minion_2
 
@@ -129,113 +53,108 @@ def count_damage(warband):
 	return damage
 
 
+def play_first():
 
-if len(Player_1.warband) > len(Player_2.warband):
-	
-	p1 = Player_1.warband
-	p2 = Player_2.warband
-
-	Player_1.warband = copy.deepcopy(p1)
-	Player_2.warband = copy.deepcopy(p2)
-
-
-elif len(Player_1.warband) < len(Player_2.warband):
-	
-	p1 = Player_2.warband
-	p2 = Player_1.warband
-
-	Player_1.warband = copy.deepcopy(p1)
-	Player_2.warband = copy.deepcopy(p2)
-
-else:
-	
-	p1 = random.choice([Player_2.warband, Player_2.warband])
-
-
-	if p1 == Player_1.warband:
+	if len(Player_1.warband) > len(Player_2.warband):
+		
+		p1 = Player_1.warband
 		p2 = Player_2.warband
-	else:
+
+		Player_1.warband = copy.deepcopy(p1)
+		Player_2.warband = copy.deepcopy(p2)
+
+	elif len(Player_1.warband) < len(Player_2.warband):
+
+		
+		p1 = Player_2.warband
 		p2 = Player_1.warband
 
-
-#game
-index = 0
-factor = 1
-
-first_player_idx, second_player_idx = 0, 0
-
-next_minion = first_player_idx
-
-game = [p1, p2]
-
-while p1 and p2:
-
-	random_minion = random.randint(0, len(game[index + factor]) - 1)
-
-	minion_1 = game[index][next_minion]
-	minion_2 = game[index + factor][random_minion]
-
-	minion_1, minion_2 = attack(minion_1, minion_2)
-
-	if minion_1.life <= 0:
-		del game[index][next_minion]
-		next_minion -= 1
-
-	if minion_2.life <= 0:
-		del game[index + factor][random_minion]
-
-	next_minion += 1
-
-	if index == 0:
-		index += 1
-		factor -= 2
-
-		first_player_idx = next_minion
-
-		if second_player_idx >= len(game[1]):
-			second_player_idx = 0
-
-		next_minion = second_player_idx
+		Player_1.warband = copy.deepcopy(p1)
+		Player_2.warband = copy.deepcopy(p2)
 
 	else:
-		index -= 1
-		factor += 2
-		second_player_idx = next_minion
 		
-		if first_player_idx >= len(game[0]):
-			first_player_idx = 0
+		p1 = random.choice([Player_2.warband, Player_2.warband])
+		print("goni")
 
-		next_minion = first_player_idx
+		if p1 == Player_1.warband:
+			p2 = Player_2.warband
+		else:
+			p2 = Player_1.warband
 
-	if not p1 and not p2:
-		print("NO WINNER")
-		damage = 0
-
-	elif not p2:
-		print(Player_1.name, "WINNER")
-		damage = count_damage(p1)
-
-	elif not p1:
-		print(Player_2.name, "WINNER")
-		damage = count_damage(p2)
-
-print("DAMAGE: ", damage)
+	return p1, p2
 
 
-# print()
-# for z in p1:
-# 	print(z.name)
 
-# print()
-# for z in p2:
-# 	print(z.name)
-# print()
+p1, p2 = play_first()
 
+def combat():
 
-# for z in Player_1.warband:
-# 	print(z.name)
+	#game
+	index = 0
+	factor = 1
 
-# print()
+	first_player_idx, second_player_idx = 0, 0
 
-# for z in Player_2.warband:
-# 	print(z.name)
+	next_minion = first_player_idx
+
+	game = [p1, p2]
+
+	while p1 and p2:
+
+		# if game[index + factor].is_taunted() == True:
+			# write new block
+			# pass
+		# else:
+		random_minion = random.randint(0, len(game[index + factor]) - 1)
+
+		minion_1 = game[index][next_minion]
+		minion_2 = game[index + factor][random_minion]
+
+		minion_1, minion_2 = attack(minion_1, minion_2)
+
+		if minion_1.life <= 0:
+			del game[index][next_minion]
+			next_minion -= 1
+
+		if minion_2.life <= 0:
+			del game[index + factor][random_minion]
+
+		next_minion += 1
+
+		if index == 0:
+			index += 1
+			factor -= 2
+
+			first_player_idx = next_minion
+
+			if second_player_idx >= len(game[1]):
+				second_player_idx = 0
+
+			next_minion = second_player_idx
+
+		else:
+			index -= 1
+			factor += 2
+			second_player_idx = next_minion
+			
+			if first_player_idx >= len(game[0]):
+				first_player_idx = 0
+
+			next_minion = first_player_idx
+
+		if not p1 and not p2:
+			print("NO WINNER")
+			damage = 0
+
+		elif not p2:
+			print(Player_1.name, "WINNER")
+			damage = count_damage(p1)
+
+		elif not p1:
+			print(Player_2.name, "WINNER")
+			damage = count_damage(p2)
+
+	print("DAMAGE: ", damage)
+
+combat()
---
+++ minions.py
@@ -0,0 +1,78 @@
+from enum import Enum
+
+class Player:
+
+	def __init__(self, name, warband):
+		self.name = name
+		self.warband = warband
+		# type of warband -> list
+
+class MinionType(Enum):
+
+	PLAIN = 0
+	MURLOC = 1
+	DRAGON = 2
+	BEAST = 3
+	MECH = 4
+	DEMON = 5
+
+class Card:
+	name = None
+	life = None
+	attack = None
+	tier = None
+
+	def __init__(self, name, attack, life, tier, m_type):
+		self.name = name
+		self.attack = attack
+		self.life = life
+		self.tier = tier
+		self.m_type = m_type
+
+
+class Event:
+	def __init__(self, evnt):
+		self.evnt = evnt
+
+def Taunt(Event):
+
+	def is_attacked_first(self):
+		attacked_first = True
+
+
+#CHECKCHEKCCHECKCHEKCHCKLECHEKCHEKCHEKCHEKCHECK
+
+
+class AddingAttack(Event):
+	# murloc_warleader
+
+	is_alive = True
+	is_attack_added = False
+
+	def __init__(self, is_alive, additional_attack, objects_to_whom):
+		
+		for i in self.objects_to_whom:
+			i.attack += additional_attack
+
+		is_attack_added = True
+
+class DoublingAttack(Event):
+	# glyph guardian
+
+	is_attacking = True
+
+	def __init__(self, attack):
+		self.attack = 2*attack
+
+
+
+class StartOfCombat(Event):
+	# red whelp
+	is_attacking = True
+
+	def __init__(self, warband):
+		damage = sum(1 for i in self.warband if i.m_type == 2)
+
+
+
+
