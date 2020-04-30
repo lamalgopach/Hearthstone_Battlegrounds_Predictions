7d74e3047c3b7b5a7536e3b9bff05a5b910685de
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2020-04-29 20:02:11 -0700

    Selfless Hero deathrattle added.

--- combat.py
+++ combat.py
@@ -1,54 +1,32 @@
 import random
 from random import choice
 import copy
-from minions import Card, Player
+from minions import Card, Player, minions
 
+alices_warband = [] 
+bobs_warband = []
 
+for minion in minions:
 
-# classes written:
-murloc_warleader = Card("Murloc Warleader", 3, 3, 2, 1, False, False)
-glyph_guardian = Card("Glyph Guardian", 2, 4, 2, 2, False, False)
-red_whelp = Card("Red Whelp", 1, 2, 1, 2, False, False)
+	s = random.choice([1, 2])
 
+	if s == 2:
+		alices_warband.append(minion)
+	else:
+		bobs_warband.append(minion)
 
-#taunt:
-dragonspawn_lieutenant = Card("Dragonspawn Lieutenant", 2, 3, 1, 2, True, False)
-# + divine shield
-righteous_protector = Card("Righteous Protector", 1, 1, 1, 0, True, True)
-
-
-
-#deathrattles:
-spawn_of_nzoth = Card("Spawn Of n'Zoth", 2, 2, 2, 0, False, False)
-infested_wolf = Card("Infested Wolf", 3, 3, 3, 3, False, False)
-selfless_hero = Card("Selfless Hero", 2, 1, 1, 0, False, False)
-
-# battlecry:
-rockpool_hunter = Card("Rockpool Hunter", 2, 3, 1, 1, False, False)
-red_whelp_a = Card("Red Whelp", 1, 2, 1, 2, False, False)
-red_whelp_b = Card("Red Whelp", 1, 2, 1, 2, False, False)
-red_whelp_c = Card("Red Whelp", 1, 2, 1, 2, False, False)
-
-alices_warband = [dragonspawn_lieutenant, red_whelp_a, selfless_hero] 
-bobs_warband = [righteous_protector, red_whelp_b, spawn_of_nzoth]
 
 Player_1 = Player("Alice", alices_warband)
 Player_2 = Player("Bob", bobs_warband)
 
 
+def attack(minion1, minion2):
+	minion1.take_damage(minion2.attack)
+	minion2.take_damage(minion1.attack)
 
-def attack(minion_1, minion_2):
-
-
-	minion_1.take_damage(minion_2.attack)
-	minion_2.take_damage(minion_1.attack)
-	print(minion_1.has_ds, "taak")
-	print(minion_2.has_ds, "ahhak")
-
-	return minion_1, minion_2
+	return minion1, minion2
 
 def count_damage(warband):
-
 	damage = 0
 
 	for minion in warband:
@@ -57,7 +35,7 @@ def count_damage(warband):
 	return damage
 
 
-def play_first():
+def game_order():
 
 	order = True
 
@@ -94,26 +72,22 @@ def count_taunts(px):
 	return output, taunted_minions
 
 
-p1, p2, game = play_first()
+p1, p2, game = game_order()
 
 def combat(p1, p2, game):
-
-	#game
-	index = 0
-	factor = 1
-
+	index, factor = 0, 1
 	first_player_idx, second_player_idx = 0, 0
 
 	next_minion = first_player_idx
 
-
 	while p1 and p2:
 
 
-		taunts_num, taunts = count_taunts(game[index + factor])
+		# taunts_num, taunts = count_taunts(game[index + factor])
 		attacked_minion = None
 
-		if taunts_num > 0:
+		if count_taunts(game[index + factor])[0] > 0:
+			taunts = count_taunts(game[index + factor])[1]
 			r = random.randint(0, len(taunts) - 1)
 			minion = taunts[r]
 
@@ -125,32 +99,33 @@ def combat(p1, p2, game):
 		else:
 			attacked_minion = random.randint(0, len(game[index + factor]) - 1)
 
-
-
 		minion_1 = game[index][next_minion]
 		minion_2 = game[index + factor][attacked_minion]
-
-
-
-		print()
-
-		print(minion_1.name, minion_1.attack, minion_1.health)
-		print(minion_2.name, minion_2.attack, minion_2.health)
+		
+		# print()
+		# print(minion_1.name, minion_1.attack, minion_1.health)
+		# print(minion_2.name, minion_2.attack, minion_2.health)
 
 		minion_1, minion_2 = attack(minion_1, minion_2)
 
-		print(minion_1.name, minion_1.attack, minion_1.health)
-		print(minion_2.name, minion_2.attack, minion_2.health)
-		print(minion_2.health, minion_2.name)
-		print(p1)
-		print(p2)
+		# print(minion_1.name, minion_1.attack, minion_1.health)
+		# print(minion_2.name, minion_2.attack, minion_2.health)
+		# print(minion_2.health, minion_2.name)
+		# print(p1)
+		# print(p2)
 
 
 		if minion_1.health <= 0:
+			if minion_1.has_deathrattle:
+				random_m = random.choice(game[index])
+				minion_1.deathrattle(random_m)
 			del game[index][next_minion]
 			next_minion -= 1
 
 		if minion_2.health <= 0:
+			if minion_2.has_deathrattle:
+				random_m = random.choice(game[index + factor])
+				minion_2.deathrattle(random_m)
 			del game[index + factor][attacked_minion]
 
 
@@ -177,27 +152,16 @@ def combat(p1, p2, game):
 
 			next_minion = first_player_idx
 
-		if not p1 and not p2:
-			print("NO WINNER")
-			damage = 0
+	if not p1 and not p2:
+		print("NO WINNER")
+		damage = 0
 
 
-		elif not p1 or not p2:
-			winner = Player_1.name if p1 else Player_2.name 
-			print(f'{winner} WINNER')
-			
-			damage = count_damage(p1) if p1 else count_damage(p2)
-			print(f'DAMAGE: {damage}')
-
-
-		# elif not p2:
-		# 	print(Player_1.name, "WINNER")
-		# 	damage = count_damage(p1)
-
-		# elif not p1:
-		# 	print(Player_2.name, "WINNER")
-		# 	damage = count_damage(p2)
-
-	# print("DAMAGE: ", damage)
+	else:
+		winner = Player_1.name if p1 else Player_2.name 
+		print(f'{winner} WINNER')
+		
+		damage = count_damage(p1) if p1 else count_damage(p2)
+		print(f'DAMAGE: {damage}')
 
 combat(p1, p2, game)
--- minions.py
+++ minions.py
@@ -18,8 +18,7 @@ class MinionType(Enum):
 
 class Card:
 
-
-	def __init__(self, name, attack, health, tier, m_type, is_taunted, has_ds):
+	def __init__(self, name, attack, health, tier, m_type, is_taunted, has_ds, has_deathrattle):
 		self.name = name
 		self.attack = attack
 		self.health = health
@@ -27,6 +26,7 @@ class Card:
 		self.m_type = m_type
 		self.is_taunted = is_taunted
 		self.has_ds = has_ds
+		self.has_deathrattle = has_deathrattle
 
 
 	def take_damage(self, damage):
@@ -35,55 +35,92 @@ class Card:
 
 		if self.has_ds:
 			self.has_ds = False
-			print("goslaw")
 		else:
 			self.health -= damage
-
 
 	def remove_ds(self):
 		self.has_ds = False
 
 
+class SelflessHero(Card):
+
+	def deathrattle(self, minion):
+		minion.has_ds = True
 
 
-class Event:
-	def __init__(self, evnt):
-		self.evnt = evnt
+
+# types of cards:
+# minions completed:
+
+dragonspawn_lieutenant = Card("Dragonspawn Lieutenant", 2, 3, 1, 2, True, False, False)
+righteous_protector = Card("Righteous Protector", 1, 1, 1, 0, True, True, False)
+
+
+# classes written:
+murloc_warleader = Card("Murloc Warleader", 3, 3, 2, 1, False, False, False)
+glyph_guardian = Card("Glyph Guardian", 2, 4, 2, 2, False, False, False)
+red_whelp = Card("Red Whelp", 1, 2, 1, 2, False, False, False)
+
+#deathrattles:
+spawn_of_nzoth = Card("Spawn Of n'Zoth", 2, 2, 2, 0, False, False, False)
+infested_wolf = Card("Infested Wolf", 3, 3, 3, 3, False, False, False)
+selfless_hero = SelflessHero("Selfless Hero", 2, 1, 1, 0, False, False, True)
+
+# battlecry:
+rockpool_hunter = Card("Rockpool Hunter", 2, 3, 1, 1, False, False, False)
+
+minions = [dragonspawn_lieutenant, righteous_protector, murloc_warleader,
+			glyph_guardian, red_whelp, spawn_of_nzoth, infested_wolf, selfless_hero, 
+			rockpool_hunter]
+
+
+
+
+
+
+
+
+
+
+
+# class Event:
+# 	def __init__(self, evnt):
+# 		self.evnt = evnt
 
 
 
 #CHECKCHEKCCHECKCHEKCHCKLECHEKCHEKCHEKCHEKCHECK
 
 
-class AddingAttack(Event):
-	# murloc_warleader
+# class AddingAttack(Event):
+# 	# murloc_warleader
 
-	is_alive = True
-	is_attack_added = False
+# 	is_alive = True
+# 	is_attack_added = False
 
-	def __init__(self, is_alive, additional_attack, objects_to_whom):
+# 	def __init__(self, is_alive, additional_attack, objects_to_whom):
 		
-		for i in self.objects_to_whom:
-			i.attack += additional_attack
+# 		for i in self.objects_to_whom:
+# 			i.attack += additional_attack
 
-		is_attack_added = True
+# 		is_attack_added = True
 
-class DoublingAttack(Event):
-	# glyph guardian
+# class DoublingAttack(Event):
+# 	# glyph guardian
 
-	is_attacking = True
+# 	is_attacking = True
 
-	def __init__(self, attack):
-		self.attack = 2*attack
+# 	def __init__(self, attack):
+# 		self.attack = 2*attack
 
 
 
-class StartOfCombat(Event):
-	# red whelp
-	is_attacking = True
+# class StartOfCombat(Event):
+# 	# red whelp
+# 	is_attacking = True
 
-	def __init__(self, warband):
-		damage = sum(1 for i in self.warband if i.m_type == 2)
+# 	def __init__(self, warband):
+# 		damage = sum(1 for i in self.warband if i.m_type == 2)
 
 
 
