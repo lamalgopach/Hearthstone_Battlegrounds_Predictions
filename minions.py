46d2c8eec0542036ea5fe9e208ef554c6b25bce6
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2020-05-05 16:49:11 -0700

    Added new classes: Dragonspawn Lieutant, Murloc Warleader, Righteous Protector, Rockpool Hunter. Changed classes orded. Init added to each classes. Changed is_taunted, has_ds, has_deathrattle to default False in Card's init.

--- minions.py
+++ minions.py
@@ -19,8 +19,8 @@ class MinionType(Enum):
 
 class Card:
 
-	def __init__(self, name, attack, health, tier, m_type, is_taunted, has_ds, 
-		has_deathrattle):
+	def __init__(self, *, name, attack, health, tier, m_type, is_taunted=False, 
+		has_ds=False, has_deathrattle=False):
 		self.name = name
 		self.attack = attack
 		self.health = health
@@ -29,6 +29,9 @@ class Card:
 		self.is_taunted = is_taunted
 		self.has_ds = has_ds
 		self.has_deathrattle = has_deathrattle
+
+
+	# deathrattle = None
 
 
 	def take_damage(self, damage):
@@ -50,29 +53,35 @@ class Card:
 		return friendly_minions
 
 
-class SelflessHero(Card):
 
-	def deathrattle(self, friendly_minions, j):
-		if not friendly_minions:
-			return
 
-		minion = random.choice(friendly_minions)
-		minion.has_ds = True
-		return minion
+class DragonspawnLieutenant(Card):
 
-class SpawnOfnZoth(Card):
+	def __init__(self):
+		super().__init__(name="Dragonspawn Lieutenant", attack=2, health=3, tier=1, m_type=2, 
+			is_taunted=True)
 
-	def deathrattle(self, friendly_minions, j):
-		if friendly_minions:
-			for minion in friendly_minions:
-				minion.attack += 1
-				minion.health += 1
-		return friendly_minions	
+
+
+class GlyphGuardian(Card):
+
+	def __init__(self):
+		super().__init__(name="Glyph Guardian", attack=2, health=4, tier=2, m_type=2)
+
+	def add_attack(self):
+		#change it
+		self.attack = 2 * self.attack
+
 
 class InfestedWolf(Card):
 
+	def __init__(self):
+		super().__init__(name="Infested Wolf", attack=3, health=3, tier=3, m_type=3, 
+			has_deathrattle=True)
+
 	def deathrattle(self, friendly_minions, j):
 		spider = Card("Spider", 1, 1, 1, 3, False, False, False)
+		#are you sure?
 		friendly_minions.insert(j, spider)
 
 		if len(friendly_minions) < 6:
@@ -81,22 +90,18 @@ class InfestedWolf(Card):
 
 		return friendly_minions
 
-class GlyphGuardian(Card):
+class MurlocWarleader(Card):
 
-	def add_attack(self):
-		self.attack = 2 * self.attack
+	def __init__(self):
+		super().__init__(name="Murloc Warleader", attack=3, health=3, tier=2, m_type=1)
+
 
 
 class RedWhelp(Card):
 
-	# name = "RedWhelp"
-	# attack = 1
-	# health = 2
-	# tier = 1
-	# m_type = 2
-	# is_taunted = False
-	# has_ds = False
-	# has_deathrattle = False
+	def __init__(self):
+		super().__init__(name="Red Whelp", attack=1, health=2, tier=1, m_type=2)
+
 
 	def take_no_damage(self):
 		self.has_ds = True
@@ -108,31 +113,79 @@ class RedWhelp(Card):
 			if minion.m_type == 2:
 				damage += 1
 		self.attack = damage
-		self.take_no_damage()
+		# self.take_no_damage()
+		self.has_ds = True
+
+
+
+class RighteousProtector(Card):
+	def __init__(self):
+		super().__init__(name="Righteous Protector", attack=1, health=1, tier=1, m_type=0,
+			is_taunted=True, has_ds =True)
+
+
+class RockpoolHunter(Card):
+	def __init__(self):
+		super().__init__(name="Rockpool Hunter", attack=2, health=3, tier=1, m_type=1)
+
+
+class SelflessHero(Card):
+	def __init__(self):
+		super().__init__(name="Selfless Hero", attack=2, health=1, tier=1, m_type=0, 
+			has_deathrattle=True)
+	def deathrattle(self, friendly_minions, j):
+		if not friendly_minions:
+			return
+
+		minion = random.choice(friendly_minions)
+		minion.has_ds = True
+		return minion
+
+class SpawnOfnZoth(Card):
+	def __init__(self):
+		super().__init__(name="Spawn Of n'Zoth", attack=2, health=2, tier=2, m_type=2, 
+			has_deathrattle=True)
+	def deathrattle(self, friendly_minions, j):
+		if friendly_minions:
+			for minion in friendly_minions:
+				minion.attack += 1
+				minion.health += 1
+		return friendly_minions	
+
+# Jakub:
+# minion = SpawnOfnZoth()
+# if minion.deathrattle is not None:
+# # if hasattr(minion, "deathrattle"):
+# 	minion.deathrattle(friendly_minions, j)
+
+
+# Jakub
+# class Minion:
+# 	def __init__(self, *, attack_value):
+# 		self.attack_value = attack_value
+
+# 	def attack(self, game_state, target):
+# 		target.health -= self.attack_value
+# 		self.health -= target.attack_value
+# 		return game_state
+
+# class GlyphGuardian(Minion):
+# 	def attack(*args, **kwargs):
+# 		self.attack_value *= 2
+# 		super().attack(*args, **kwargs)
 
 
 
 
-# minions completed:
-dragonspawn_lieutenant = Card("Dragonspawn Lieutenant", 2, 3, 1, 2, True, False, False)
-righteous_protector = Card("Righteous Protector", 1, 1, 1, 0, True, True, False)
-spawn_of_nzoth = SpawnOfnZoth("Spawn Of n'Zoth", 2, 2, 2, 0, False, False, True)
-selfless_hero = SelflessHero("Selfless Hero", 2, 1, 1, 0, False, False, True)
-glyph_guardian = GlyphGuardian("Glyph Guardian", 2, 4, 2, 2, False, False, False)
-infested_wolf = InfestedWolf("Infested Wolf", 3, 3, 3, 3, False, False, True)
 
-# done but need to be finished (random player order)
-red_whelp = RedWhelp("Red Whelp", 1, 2, 1, 2, False, False, False)
+#todo:
+# redwhelp: random player order)
+# glyph_guardian: change add attack
 
-# to do:
-murloc_warleader = Card("Murloc Warleader", 3, 3, 2, 1, False, False, False)
 
-# battlecry:
-rockpool_hunter = Card("Rockpool Hunter", 2, 3, 1, 1, False, False, False)
-
-minions_lst = [dragonspawn_lieutenant, righteous_protector, murloc_warleader,
-			glyph_guardian, red_whelp, spawn_of_nzoth, infested_wolf, selfless_hero, 
-			rockpool_hunter]
+# minions_lst = [dragonspawn_lieutenant, righteous_protector, murloc_warleader,
+# 			glyph_guardian, red_whelp, spawn_of_nzoth, infested_wolf, selfless_hero, 
+# 			rockpool_hunter]
 
 
 
