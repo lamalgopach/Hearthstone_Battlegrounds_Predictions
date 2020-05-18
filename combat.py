da40c8f92dedcd3aa481960e169738a93a8c0226
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2020-05-18 16:46:37 -0700

    Made warbands in Player objects as Warband objects. Used create_warband() method.

--- combat.py
+++ combat.py
@@ -1,24 +1,18 @@
 import random
 from random import choice
 import copy
-# from battle import Player, Warband, Battle, GameState
 from battle import Player, Warband, BattleState
 from minions import RedWhelp
-from creating_minions_in_warbands import create_warband
-
 
 def choose_first(player1, player2):
 	order = True
 
-	p1 = copy.deepcopy(player1.warband)
-	p2 = copy.deepcopy(player2.warband)	
+	w1 = copy.deepcopy(player1.warband)
+	w2 = copy.deepcopy(player2.warband)
 
-	w1 = Warband(Player1, p1)
-	w2 = Warband(Player2, p2)
-
-	if len(player1.warband) < len(player2.warband):
+	if len(w1.warband) < len(w2.warband):
 		order = False
-	elif len(player1.warband) == len(player2.warband):
+	elif len(w1.warband) == len(w2.warband):
 		order = random.choice([True, False])
 
 	if order == True:
@@ -28,14 +22,19 @@ def choose_first(player1, player2):
 
 	return w1, w2, game
 
-
-alices_warband = create_warband()
-bobs_warband = create_warband()
-
+# create Alices Warband as object of Warband class:
+alices_warband = Warband("Alice")
+bobs_warband = Warband("Bob")
+# use warband method to create warband with minions:
+alices_warband.create_warband([])
+bobs_warband.create_warband([])
+# create object of Player class and link this warband to this Player:
 Player1 = Player("Alice", alices_warband)
 Player2 = Player("Bob", bobs_warband)
+
 #what are players names? into console
 
+# CREATE THE ORDER OF ATTACK:
 w1, w2, game = choose_first(Player1, Player2)
 
 attacking_player = Player1 if game[0] == w1 else Player2
@@ -58,25 +57,7 @@ def combat(w1, w2):
 		print("Attacked: ", battle_state.attacked_player.name)
 
 		# assign attacked minion:
-		# attacked_minion = None
 		attacked_minion = battle_state.choose_attacked_minion()
-
-
-# ######################
-# 		if battle_state.count_taunts()[0] > 0:
-# 			taunts = battle_state.count_taunts()[1]
-# 			r = random.randint(0, len(taunts) - 1)
-# 			minion = taunts[r]
-
-# 			for i in range(len(battle_state.attacked_warband.warband)):
-# 				if battle_state.attacked_warband.warband[i].name == minion.name:
-# 					attacked_minion = i
-# 					break
-
-# 		# otherwise attacked minion is chosen randomly:
-# 		else:
-# 			attacked_minion = random.randint(0, len(battle_state.attacked_warband.warband) - 1)	
-# ###################
 
 		# create minions in game:
 		minion1 = battle_state.attacking_warband.warband[battle_state.attack_i]
@@ -148,6 +129,9 @@ def combat(w1, w2):
 	print(Player1.life, "Player1 life", Player1.name)
 	print(Player2.life, "Player2 life", Player2.name)
 
+	print(len(Player1.warband.warband))
+	print(len(Player2.warband.warband))
+
 combat(w1, w2)
 
-# May 14th: 137 lines of code
+# May 15th: 133 lines of code
