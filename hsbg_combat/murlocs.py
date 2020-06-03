160255ba82a7a48a6a1877fc7988c78e0f9e94ce
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2020-06-03 14:24:46 -0700

    Primalfin Lookout added.

--- hsbg_combat/murlocs.py
+++ hsbg_combat/murlocs.py
@@ -1,6 +1,5 @@
 from minions import *
 
-
 class ColdlightSeer(Card):
 	#btlcry
 	def __init__(self):
@@ -15,14 +14,11 @@ class FelfinNavigator(Card):
 						m_type=MinionType.MURLOC)
 
 
-
-
 class MurlocTidecaller(Card):
 # add attack
 	def __init__(self):
 		super().__init__(name="Murloc Tidecaller", attack_value=1, health=2, tier=1, 
 						m_type=MinionType.MURLOC)
-
 
 
 class MurlocTidehunter(Card):
@@ -31,10 +27,18 @@ class MurlocTidehunter(Card):
 		super().__init__(name="Murloc Tidehunter", attack_value=2, health=1, tier=1, 
 						m_type=MinionType.MURLOC)
 
+
 # class MurlocWarleader(Card):
 # 	def __init__(self):
 # 		super().__init__(name="Murloc Warleader", attack_value=3, health=3, tier=2, 
 # 			m_type=MinionType.MURLOC)
+
+
+class PrimalfinLookout(Card):
+	#Btlcry
+	def __init__(self):
+		super().__init__(name="Primalfin Lookout", attack_value=3, health=2, tier=5, 
+						m_type=MinionType.MURLOC)	
 
 
 class RockpoolHunter(Card):
@@ -56,3 +60,4 @@ class MurlocScout(Card):
 	def __init__(self):
 		super().__init__(name="Murloc Scout", attack_value=1, health=1, tier=1, 
 						m_type=MinionType.MURLOC)
+
