bbf1276a509f83d088cc62c66315a2377be80560
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2020-06-02 20:23:33 -0700

    murlocs.py added. MurlocTidecaller(), MurlocTidehunter(), MurlocScout(),

---
+++ hsbg_combat/murlocs.py
@@ -0,0 +1,22 @@
+from minions import *
+
+class MurlocTidecaller(Card):
+# add attack
+	def __init__(self):
+		super().__init__(name="Murloc Tidecaller", attack_value=1, health=2, tier=1, 
+						m_type=MinionType.MURLOC)
+
+
+
+class MurlocTidehunter(Card):
+	#btlcry
+	def __init__(self):
+		super().__init__(name="Murloc Tidehunter", attack_value=2, health=1, tier=1, 
+						m_type=MinionType.MURLOC)
+
+
+# summoned:
+class MurlocScout(Card):
+	def __init__(self):
+		super().__init__(name="Murloc Scout", attack_value=1, health=1, tier=1, 
+						m_type=MinionType.MURLOC)
